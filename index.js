675ad3aaa21d81db122674922e94245c250b3530
Vladimir Kudinov <frux@yandex-team.ru>
2015-09-30 02:20:07 +0500

    Code

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+    'pre-commit': function(next){
+        var stagedFiles = this.stagedFilesSync('ACM'),
+            jscs = __dirname  + '/node_modules/jscs/bin/jscs',
+            jsTest = /\.js$/,
+            Uppercode = this;
+
+        stagedFiles.forEach(function(file){
+            if(jsTest.test(file)){
+                console.log('JSCS: ' + file);
+                Uppercode.exec(jscs + ' ' + file, function(err, data){
+                    if(!err){
+                        Uppercode.exec('git', ['add', file]);
+                    }
+                });
+            }
+        });
+
+        next();
+    }
+};
